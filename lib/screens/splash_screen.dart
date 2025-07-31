45e6f1e14ccd30a6813ba673f1d427fc4d2bcbc3
Fawad Ahmed <fawad@MacBook-Air-2.local>
2025-07-31 22:53:33 +0500

    added paths to newly added screens

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:movie/screens.dart/login_screnn.dart';
+import 'package:movie/screens.dart/movie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  bool isContentVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        isContentVisible = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/image.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedContainer(
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeIn,
+                width: isContentVisible ? 150 : 200,
+                height: isContentVisible ? 150 : 200,
+                child: Image.asset('assets/Icon.png'),
+              ),
+              SizedBox(height: 20),
+              AnimatedOpacity(
+                opacity: isContentVisible ? 1.0 : 0.0,
+                duration: const Duration(seconds: 2),
+                curve: Curves.easeIn,
+                child: Column(
+                  children: [
+                    Text(
+                      'Enjoy the newest movies',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF6100C2),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 100,
+                          vertical: 15,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Or',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Movie()),
+                          ),
+                          child: Text(
+                            'go to Website',
+                            style: TextStyle(
+                              color: Color(0xFF9C4DFF),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 15,
+                              fontFamily: 'Poppins',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
